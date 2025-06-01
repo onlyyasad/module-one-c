809463521e38e69f60399b5b1042e7553237b868
hafij <hafij.alasad23@gmail.com>
2025-06-01 22:20:55 +0600

    ascii to int and vice versa tested

--- class5.c
+++ class5.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 
+/**
+ * Scanf
+ * More on type specifiers (double and long long)
+ * */
+
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
 
-    int x;
+    int ascii = 80;
+    char ch = ascii;
 
-    scanf("%d", &x);
-
-    int result = x * x * x;
-
-    printf("%d\n", result);
+    printf("Char value of 87 is %c\n", ch);
 
     return 0;
 }
