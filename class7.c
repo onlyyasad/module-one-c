f545417ad1a29e2c2ab804a5f63f68b0dcdf2172
hafij <hafij.alasad23@gmail.com>
2025-06-03 00:38:26 +0600

    right shift and left shift and finding nth number bit

--- class7.c
+++ class7.c
@@ -32,10 +32,51 @@ int main(){
 
 	printf("Value of a = %d; value of b = %d; value of c = %d , value of d = %d\n", a, b, c, d);
 
-	// even odd check with bitwise operation: if return is zero then the input number is even, if return is one then the input number is odd
-	int x;
-	scanf("%d", &x);
-	int y = x & 1;
-	printf("value of zeroeth bit is: %d\n", y);
+	/** 
+	 * even odd check with bitwise operation: if return is zero then the input number is even, if return is one then the input number is odd
+	 **/
+
+	// int x;
+	// scanf("%d", &x);
+	// int y = x & 1;
+	// printf("value of zeroeth bit is: %d\n", y);
+
+	/** 
+	 * right shift: >> if we right shift once it will work as integer division with 2
+	 * example: if we right shift 11 once, the result will be 5
+	 **/
+
+	// int p = 11;
+
+	// int q = (p >> 1);
+
+	// int m;
+
+	// scanf("%d", &m);
+
+	// int bit0, bit1, bit2, bit3;
+
+	// bit0 = m & 1;
+	// bit1 = (m >> 1) & 1;
+	// bit2 = (m>>2) & 1;
+	// bit3 = (m>>3) & 1;
+
+	// printf("bit0 = %d, bit1 = %d, bit2 = %d, bit3 %d\n", bit0, bit1, bit2, bit3);
+
+
+	/**
+	 * Left shift: << if we left shift once it will work as multiplication with 2
+	 * */
+
+	int i; //number
+	int j; // bit position
+
+	scanf("%d", &i);
+	scanf("%d", &j);
+
+	int ans = i & (1 << j);
+
+	printf("value of your is %d\n", ans);
+
 	return 0;
 }
