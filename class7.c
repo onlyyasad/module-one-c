8af810fdcced07343056534fd07c86c8c4d5d205
hafij <hafij.alasad23@gmail.com>
2025-06-02 23:48:07 +0600

    even odd finder using bitwise operation

--- class7.c
+++ class7.c
@@ -21,11 +21,21 @@ int main(){
 
 	int a = 20;
 
-	a += 5;
+	int b = a++; //post-increment b value should be previous value of a
+
+	int c = 10; 
+	int d = ++c; //pre-increment d value should be 11
+
+	// a+;
 
 	// scanf("%d", &a);
 
-	printf("Test %d", a);
+	printf("Value of a = %d; value of b = %d; value of c = %d , value of d = %d\n", a, b, c, d);
 
+	// even odd check with bitwise operation: if return is zero then the input number is even, if return is one then the input number is odd
+	int x;
+	scanf("%d", &x);
+	int y = x & 1;
+	printf("value of zeroeth bit is: %d\n", y);
 	return 0;
 }
