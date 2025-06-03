1002f66a8edf430740a791036aef17f43166f6a2
hafij <hafij.alasad23@gmail.com>
2025-06-03 21:06:49 +0600

    bitwise operator rules added

--- class8.c
+++ class8.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+/**
+ * Bitwise operators:
+ * & : and -> if both zero 0 then 0 otherwise 1
+ * 		-> example: 100101
+ * 				    101100  &
+ * 				   --------
+ * 				    101101
+ * 
+ * | : or -> if both one 1 then 1 otherwise 0
+ * 		-> example: 100101
+ * 				    101100  |
+ * 				   --------
+ * 				    100100
+ * 
+ * ^ : xor(exclusive or) -> if both same then 0, if different then 1
+ * 		-> example: 100101
+ * 				    101100  ^
+ * 				   --------
+ * 				    001001
+ * */
+
 int main (){
 	#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
