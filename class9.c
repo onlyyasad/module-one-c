0d427faa0bec207e8306b1c4713fae0dc3719ff4
hafij <hafij.alasad23@gmail.com>
2025-06-05 20:06:29 +0600

    class 9 file initialized

---
+++ class9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * Topic:
+ * Contitional Statements
+ * Logical operators
+ * */
+
+int main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int x;
+
+	scanf("%d", &x);
+
+	printf("Test input %d\n", x);
+
+	return 0;
+}
